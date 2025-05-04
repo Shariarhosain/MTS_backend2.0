d22c5cc0c489ff3fcef6f753f72e62463007b06a
Md. Shariar Hosain Sanny <76251576+Shariarhosain@users.noreply.github.com>
2025-05-04 10:34:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(err.status || 500).json({ message: err.message || 'Something went wrong.' });
 });
-
+app.listen(3500, '0.0.0.0', () => {
+  console.log(`Server is running on port`);
+});
 
 
